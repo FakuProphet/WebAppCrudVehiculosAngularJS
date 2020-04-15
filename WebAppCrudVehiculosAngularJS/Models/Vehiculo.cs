94cc568203ccb83218adc9df1728385cb8ac7c8a
Faku <Faku@DESKTOP-VTKSBSM>
2020-04-15 00:02:49 -0300

    save

---
+++ WebAppCrudVehiculosAngularJS/Models/Vehiculo.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+namespace WebAppCrudVehiculosAngularJS.Models
+{
+    public class Vehiculo
+    {
+        [Key]
+        public int id { get; set; }
+        public string marca { get; set; }
+        public string nombre { get; set; }
+        public string color { get; set; }
+        public string anio { get; set; }
+    }
+}
